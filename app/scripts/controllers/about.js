0d26f787e56abe81219fd370d85bfd4c9bd34a09
gugol2 <googolgugol@gmail.com>
2016-07-08 11:09:51 +0200

    first commit, project just scaffolded with yo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFinancialPortalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularFinancialPortalApp
+ */
+angular.module('angularFinancialPortalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFinancialPortalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularFinancialPortalApp
+ */
+angular.module('angularFinancialPortalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
