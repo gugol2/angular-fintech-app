7e930bd71491ea4de84de05bdf160d81c9060413
gugol2 <googolgugol@gmail.com>
2016-07-13 14:18:49 +0200

    removed protocol from 'API_URL' constant, it's better like this (gives more clues if the call fails)

--- app/scripts/services/apiconstants.js
+++ app/scripts/services/apiconstants.js
@@ -18,7 +18,7 @@
 				'JsonStub-User-Key' : '9facef2e-9583-4a83-9f08-c87159f1c113',
 				'JsonStub-Project-Key' : '6ed070c1-b334-4612-8fa8-169c5e45baef'
 			},
-			'API_URL' : 'http://jsonstub.com/etsfintech/symbols',
+			'API_URL' : '//jsonstub.com/etsfintech/symbols',
 			'API_DATA_PATTERN_REGION' : /(region_level\d*)/,
 			'API_DATA_PATTERN_RISK_FAMILY' : /(sub_family)/,
 			'API_DATA_PATTERN_SECTOR' : /(sector_level\d*)/,
